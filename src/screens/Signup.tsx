6ab49f4bfded7ffd6b262eeaef06bd0a16d238e1
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-01 17:46:43 +0530

    AuthenticationPage

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+export default function Signup() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Signup</Text>
     </View>
   )
 }
