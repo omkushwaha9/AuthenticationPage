8d9c028579041a89e86eb6ecf09890aa412e37cd
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-02 14:43:40 +0530

    SignUpFile

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,3 +1,4 @@
+// SignUpFile
 import { StyleSheet, Text, View } from 'react-native'
 import React, {useContext, useState}  from 'react'
 //React native elements
@@ -9,6 +10,10 @@ import{AppwriteContext} from '../Appwrite/AppwriteContext'
 // Navigation
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
 import {AuthStackParamList} from '../routes/AuthStack'
+import { KeyboardAvoidingView } from 'react-native'
+import { Platform } from 'react-native'
+import { TextInput } from 'react-native'
+import { Pressable } from 'react-native'
 
 type SignupScrenProps = NativeStackScreenProps<AuthStackParamList,
 'Signup'>
@@ -16,7 +21,7 @@ const {appwrite, setIsLoggedIn} = useContext(AppwriteContext)
 
 
 
-const Signup = () => {
+const Signup = ({navigation}:SignupScrenProps) => {
   const {appwrite, setIsLoggedIn} = useContext(AppwriteContext)
 
   const[error, setError] = useState<string>('')
@@ -61,12 +66,169 @@ const Signup = () => {
     
   }
   return (
-    <View>
-      <Text>Signup</Text>
+    <KeyboardAvoidingView
+    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    style={styles.container}>
+    <View style={styles.formContainer}>
+      <Text style={styles.appName}>Appwrite Auth</Text>
+
+      {/* Name */}
+      <TextInput
+        value={name}
+        onChangeText={text => {
+          setError('');
+          setName(text);
+        }}
+        placeholderTextColor={'#AEAEAE'}
+        placeholder="Name"
+        style={styles.input}
+      />
+
+      {/* Email */}
+      <TextInput
+        value={email}
+        keyboardType="email-address"
+        onChangeText={text => {
+          setError('');
+          setEmail(text);
+        }}
+        placeholderTextColor={'#AEAEAE'}
+        placeholder="Email"
+        style={styles.input}
+      />
+
+      {/* Password */}
+      <TextInput
+        value={password}
+        onChangeText={text => {
+          setError('');
+          setPassword(text);
+        }}
+        placeholderTextColor={'#AEAEAE'}
+        placeholder="Password"
+        secureTextEntry
+        style={styles.input}
+      />
+
+      {/* Repeat password */}
+      <TextInput
+        secureTextEntry
+        value={repeatpassword}
+        onChangeText={text => {
+          setError('');
+          setRepeatPassword(text);
+        }}
+        placeholderTextColor={'#AEAEAE'}
+        placeholder="Repeat Password"
+        style={styles.input}
+      />
+
+      {/* Validation error */}
+      {error ? <Text style={styles.errorText}>{error}</Text> : null}
+
+      {/* Signup button */}
+      <Pressable
+        onPress={handleSignup}
+        style={[styles.btn, {marginTop: error ? 10 : 20}]}>
+        <Text style={styles.btnText}>Sign Up</Text>
+      </Pressable>
+
+      {/* Login navigation */}
+      <Pressable
+        onPress={() => navigation.navigate('Login')}
+        style={styles.loginContainer}>
+        <Text style={styles.haveAccountLabel}>
+          Already have an account?{'  '}
+          <Text style={styles.loginLabel}>Login</Text>
+        </Text>
+      </Pressable>
     </View>
-  )
+  </KeyboardAvoidingView>
+);
+  
 }
 
 export default Signup
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  formContainer: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    height: '100%',
+  },
+  appName: {
+    color: '#f02e65',
+    fontSize: 40,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#fef8fa',
+    padding: 10,
+    height: 40,
+    alignSelf: 'center',
+    borderRadius: 5,
+
+    width: '80%',
+    color: '#000000',
+
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 1,
+  },
+  errorText: {
+    color: 'red',
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  btn: {
+    backgroundColor: '#ffffff',
+    padding: 10,
+    height: 45,
+
+    alignSelf: 'center',
+    borderRadius: 5,
+    width: '80%',
+    marginTop: 10,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 3,
+  },
+  btnText: {
+    color: '#484848',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  loginContainer: {
+    marginTop: 60,
+  },
+  haveAccountLabel: {
+    color: '#484848',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  loginLabel: {
+    color: '#1d9bf0',
+  },
+})
